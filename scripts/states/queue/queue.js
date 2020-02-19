e2c49d60513b9a4679753329af3c31f6481d896b
Spamford <56346213+Spamford@users.noreply.github.com>
2020-02-19 14:51:24 +0000

    test

--- scripts/states/queue/queue.js
+++ scripts/states/queue/queue.js
@@ -15,3 +15,23 @@
     });
 
 })();
+
+function weaponBox() {
+
+
+
+var myArray = [
+"Oil Slick",
+"Smart Bomb",
+"Puncture"
+];
+
+var randomWeapon = myArray[Math.floor(Math.random()*myArray.length)]; 
+  
+document.getElementById('weapon').innerHTML = randomWeapon;
+
+
+setInterval(weaponBox, 5000)
+
+}
+
