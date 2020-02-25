10da2a78eee1e732da9d8aae33bd83d3fa8d626a
Spamford <56346213+Spamford@users.noreply.github.com>
2020-02-25 15:27:05 +0000

    j

--- scripts/app.js
+++ scripts/app.js
@@ -15,7 +15,7 @@ angular.module('myApp').config(['$stateProvider','$urlRouterProvider', function(
     $urlRouterProvider.otherwise("/onboarding");
     // sets up all the other states of the app
     $stateProvider
-        /*.state('onboarding', {
+        .state('onboarding', {
             url: '/onboarding',
             Url: '/onboarding?uuid&brokerHost&brokerPort&username&password&ssl',
             templateUrl : 'scripts/states/onboarding/onboarding.html',
@@ -69,83 +69,4 @@ angular.module('myApp').config(['$stateProvider','$urlRouterProvider', function(
 angular.module("myApp").run(function ($rootScope, $state, $stateParams) {
     $rootScope.$state = $state;
     $rootScope.$stateParams = $stateParams;
-});*/
-    //states
-    var onboardingState = {
-        name: 'onboarding',
-        url: '/onboarding?uuid&brokerHost&brokerPort&username&password&ssl',
-        params: {
-            uuid : {
-                dynamic: false
-            },
-            brokerHost: {
-                dynamic: false
-            },
-            brokerPort: {
-                dynamic: false
-            },
-            username: {
-                dynamic : false
-            },
-            password: {
-                dynamic : false
-            },
-            ssl:{
-                dynamic : false
-            }
-
-        },
-        templateUrl: 'scripts/states/onboarding/onboarding.html',
-        controller: "onboardingCtrl",
-        controllerAs: "onboarding",
-        resolve: {
-            broker: ['$stateParams','brokerDetails', function ($stateParams,brokerDetails) {
-            
-                if($stateParams.uuid) brokerDetails.UUID = $stateParams.uuid;
-                if($stateParams.brokerHost) brokerDetails.HOST = $stateParams.brokerHost;
-                if($stateParams.brokerPort) brokerDetails.PORT = $stateParams.brokerPort;
-                if($stateParams.username) brokerDetails.USERNAME = $stateParams.username;
-                if($stateParams.password) brokerDetails.PASSWORD = $stateParams.password;
-                if($stateParams.ssl) brokerDetails.SSL = ($stateParams.ssl.toLowerCase() == 'true');
-            }]
-        }
-    }
-
-    var casualState = {
-        name: 'casual',
-        url: '/casual',
-        templateUrl: 'scripts/states/casual/casual.html',
-        controller: 'casualCtrl',
-        controllerAs: 'casual',
-        params: {
-            channel: null,
-            ip_address: null
-        },
-        resolve: {
-            parameters: ['$q', '$state','$stateParams', function ($q, $state,$stateParams) {
-                var deferred = $q.defer();
-               
-                if ($stateParams.channel === null) {
-                    $state.transitionTo('onboarding', {});
-                }else{
-                    deferred.resolve();
-                }
-
-                return deferred.promise;
-            }]
-        }
-    };
-
-    $stateProvider.state(onboardingState);
-    $stateProvider.state(casualState);
-
-    $urlRouterProvider.otherwise('/onboarding');
-
-
-angular.module('app').run(run);
-run.$inject = [
-]
-
-function run() {
-    console.log('Mobile Scalextric 3 by James Sweetland, Sam Lord, Olly Taylor and Nathan Butler');
-}
+});
