bfeaee095f2611051ff3b3f58975bfde04736279
Spamford <56346213+Spamford@users.noreply.github.com>
2020-02-25 13:13:11 +0000

    t

--- scripts/states/casual/casual.controller.js
+++ scripts/states/casual/casual.controller.js
@@ -6,11 +6,121 @@
     .module('app.casualState')
     .controller('casualCtrl', casualCtrl);
 
-    casualCtrl.$inject = [];
+    casualCtrl.$inject = [
+        '$scope',
+        '$state',
+        '$stateParams',
+        'mqttService',
+        'brokerDetails'
+    ];
 
-    function casualCtrl() {
-        var vm = angular.extend(this, {});
-        return vm;
+    function casualCtrl($scope, $state, $stateParams, mqttService, brokerDetails) {
+        var vm = this;
+
+        var changed = false;
+    
+        var channel = $stateParams.channel;
+    
+        //Car control starts here
+        const DEFAULT_THROTTLE = 0;
+    
+        vm.throttle = DEFAULT_THROTTLE;
+        vm.actualThrottle = DEFAULT_THROTTLE;
+        vm.resources = [];
+    
+        vm.targetChannels = Array.apply(null, {
+            length: 2
+        }).map(Function.call, Number);;
+    
+        vm.targetChannels = vm.targetChannels.filter(targetChannel => targetChannel !== channel );
+        console.log(vm.targetChannels);
+    
+        vm.targetChannel = -1;
+    
+        vm.throttleError = false;
+    
+        vm.stop = stop;
+        vm.fireSpecialWeapon = fireSpecialWeapon;
+    
+    
+    
+    
+        var throttleTopic = `${brokerDetails.UUID}/control/${channel}/throttle`;
+        var getResourcesTopic = `${brokerDetails.UUID}/resources`;
+        var resourceStateTopic = `${brokerDetails.UUID}/control/{channel}/{resourceId}/state`;
+    
+        mqttService.subscribe(throttleTopic);
+    
+        mqttService.subscribe(getResourcesTopic);
+    
+        function stop() {
+    
+            var payload = {
+                set : 0
+            }
+            mqttService.publish(throttleTopic, JSON.stringify(payload));
+            
+            mqttService.disconnect();
+            $state.transitionTo('onboarding', {});
+        }
+    
+        function fireSpecialWeapon(resourceId) {
+            let payload = {
+                state: "requested",
+                target: vm.targetChannel
+            };
+            mqttService.publish(resourceStateTopic.replace(/\{resourceId\}/, resourceId).replace(/\{channel\}/, channel), JSON.stringify(payload));
+        }
+    
+        window.onhashchange = function () {
+            if (changed) {
+                console.log('changed');
+                stop();
+            } else {
+                changed = true;
+            }
+        }
+    
+        mqttService.onMessageArrived(function (message) {
+    
+            console.log(message);
+    
+            if (message.topic === throttleTopic) {
+                var throttle  = JSON.parse(message.payloadString);
+    
+                if(throttle.hasOwnProperty("throttle")){
+                    vm.actualThrottle = throttle.throttle;
+                }
+            } else if (message.topic === getResourcesTopic) {
+                vm.resources = JSON.parse(message.payloadString);
+                vm.resources.forEach(resource => {
+    
+                    mqttService.subscribe(resourceStateTopic.replace(/\{resourceId\}/, resource.id));
+                });
+                $scope.$apply();
+            }
+    
+            if (vm.resources !== undefined) {
+                vm.resources.forEach(resource => {
+                    if (message.topic === resourceStateTopic.replace(/\{resourceId\}/, resource.id)) {
+                        console.log(message);
+                    }
+                })
+            }
+    
+        });
+    
+        $scope.$watch("casual.throttle", function (newThrottle, oldThrottle) {
+            if (newThrottle != oldThrottle) {
+                var payload = {
+                    set : newThrottle
+                }
+                mqttService.publish(throttleTopic, JSON.stringify(payload));
+            }
+        })
+        //Car control ends here
+    
     }
+    
 
 })();
--- scripts/states/onboarding/onboarding.controller.js
+++ scripts/states/onboarding/onboarding.controller.js
@@ -5,12 +5,58 @@
         .module('app.onboardingState')
         .controller('onboardingCtrl', onboardingCtrl);
 
-    onboardingCtrl.$inject = [];
+    onboardingCtrl.$inject = [
+        '$rootScope',
+        '$state',
+        /*'mqttService',*/
+        'brokerDetails'
+    ];
     
-    function onboardingCtrl(
-        $state,
-    ) {
-        var vm = angular.extend(this, {});        
+    function onboardingCtrl($rootScope, $state, /*mqttService,*/ brokerDetails) {
+        var vm = this;
+
+        vm.channels = Array.apply(null, {
+            length: 2
+        }).map(Function.call, Number);
+        vm.channel = 0;
+        
+        console.log(brokerDetails);
+        /*
+    vm.go = go;
+
+    function go(valid) {
+        if (!valid) {
+            alert("Invalid Details")
+        } else {
+            mqttService.initialize(brokerDetails.HOST, brokerDetails.PORT);
+            mqttService.onConnectionLost(function () {
+                console.error("connection lost");
+            });
+
+
+            var mqttOptions = {};
+
+            if (brokerDetails.SSL) { mqttOptions.useSSL = brokerDetails.SSL; }
+            if (brokerDetails.USERNAME) {
+                mqttOptions.userName = brokerDetails.USERNAME;
+                if(brokerDetails.PASSWORD){
+                    mqttOptions.password = brokerDetails.PASSWORD;
+                }
+            }
+            mqttService.connect(function (success, error) {
+                if (success) {
+                    $state.transitionTo('casual',
+                        {
+                            channel: vm.channel,
+                        });
+                } else if (error) {
+                    console.log(error)
+                    alert(`Could Not Connect to ${brokerDetails.HOST}:${brokerDetails.PORT}`)
+                }
+
+            },mqttOptions)
+        }
+    }*/
 
         
         vm.goToQueue = function(){            
